f1a52365b9522ac3e81ea44ab6a4af36566929e3
Dhamaris Calero <calerodha0202@gmail.com>
2026-02-04 13:51:51 -0500

    Proyecto inicial

---
+++ src/proyecto_foro/modelo/mensaje.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_foro.modelo;
+
+/**
+ *
+ * @author dhaca
+ */
+public class mensaje {
+    
+}
